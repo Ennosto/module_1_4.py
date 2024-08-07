0f4235583b03f531662de8b0a505c05a11def61e
Ennosto <ennosto_me@mail.ru>
2024-08-07 12:21:03 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,16 @@
+#my_string = input('Кто ты по гороскопу? ')
+#print(len(my_string))
+#
+#my_string = input('Кто ты по гороскопу? ')
+#print(my_string.__len__())
+#
+#my_string = 'Конгломерация'
+#len(my_string)
+#print(len(my_string))
+#
+#my_string = 'Количество'
+#Kolvo = len(my_string)
+#print('Количество симоволов: ', Kolvo)
+#
+my_string = 'Треугольник'
+print(my_string.__len__())
